30ab7a7cb6b36f26e5e87de4cffb6504af416fa1
Ashutosh <ashutoshpriyadarshiardor@gmail.com>
2026-03-07 21:25:48 +0530

    release fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
     }
     compileOptions {
